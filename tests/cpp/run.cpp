36d60ebdbc49673b15397fc719d8c381beff8e3f
TheLazyCat00 <manuel.stieger@proton.me>
2026-04-02 02:38:40 +0200

    rem run.cpp

--- tests/cpp/run.cpp
+++
@@ -1,7 +0,0 @@
-#include "../../include/coda.hpp"
-int main (int argc, char *argv[]) {
-	Coda coda("tests/catalog/example.coda");
-	coda.order();
-	coda.save("tests/catalog/example.coda");
-	return 0;
-}
